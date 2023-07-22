d0b3cd9e5e270508b8f0adff04e26c6ae10af405
John Glynn <playingallday383@gmail.com>
2023-07-22 17:04:05 -0400

    updates

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,13 +1,17 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-    prefix: "/uv/service/",
-    bare: "https://uv.studentportal.lol/",
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
-  };
+  prefix: "/uv/service/",
+  bare: [
+    "https://uv.studentportal.lol/",
+    "https://uv.radon.games/",
+    "https://uv.holyubofficial.net/",
+    "https://tomp.app/",
+    "https://flow-os.liquid.is-a.dev/bare/"
+  ],
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
+};
